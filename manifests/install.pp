6f627dfe70f143f4657633fe16d612e795eadf4d
Adam S <asa188@sfu.ca>
2015-10-07 13:36:54 -0700

    Cleanup and Add support for Ubuntu 15.04 systemd
    Possibly supports Ubuntu 15.04 upstart as well but untested.
    Added the use of systemctl daemon-reload instead of file link for better
    systemd target handling, same effect at the moment but if someone
    changes the target this is more flexible.

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,5 +1,5 @@
 # installs startup files and scripts
-class ipset::install {
+class ipset::install($init_system = undef) {
   include ipset::params
 
   $cfg = $::ipset::params::config_path
@@ -20,7 +20,7 @@ class ipset::install {
 
   # autostart
   case $::osfamily {
-    'RedHat', 'redhat': {
+    'RedHat': {
       case $::operatingsystemrelease {
         /^6.*/: { # upstart starter
           file { '/etc/init/ipset.conf':
@@ -36,13 +36,38 @@ class ipset::install {
             group   => 'root',
             mode    => '0644',
             content => template("${module_name}/systemd.service.erb"),
-          }
-          file { '/etc/systemd/system/basic.target.wants/ipset.service':
-            ensure => link,
-            target => '/usr/lib/systemd/system/ipset.service',
+            notify  => Exec['ipset_systemctl_daemonreload'],
           }
         }
         default: { fail("Unsupported RedHat family version is ${$::operatingsystemrelease} - autostart not enabled") }
+      }
+    }
+    'Debian': {
+      case $::operatingsystem {
+        'Ubuntu': {
+          case $::operatingsystemrelease {
+            /^15\.\d+/: { # systemd / we are assuming they chose systemd for Ubuntu, at this moment we cannot detect is_systemd or use $::service_provider fact (stdlib 4.10.x) to help tell us whether we are upstart or systemd
+              if $init_system =~ /(?i:upstart)/ {
+                file { '/etc/init/ipset.conf':
+                  owner   => 'root',
+                  group   => 'root',
+                  mode    => '0644',
+                  content => template("${module_name}/init.upstart.erb"),
+                }
+              }
+              else {
+                file { '/usr/lib/systemd/system/ipset.service':
+                  owner   => 'root',
+                  group   => 'root',
+                  mode    => '0644',
+                  content => template("${module_name}/systemd.service.erb"),
+                  notify  => Exec['ipset_systemctl_daemonreload'],
+                }
+              }
+            }
+            default: { fail("Unsupported Ubuntu version is ${$::operatingsystemrelease} - autostart not enabled") }
+          }
+        }
       }
     }
     '': {  # handle bug in our Jenkins that returns empty strings for all of these when verifying
@@ -52,5 +77,9 @@ class ipset::install {
         }
     default:     { fail( "Unsupported OSFamily \"${$::osfamily}\" OS is \"${$::operatingsystem}\" major version is \"${$::operatingsystemmajrelease}\" ") }
   }
-
+  exec { 'ipset_systemctl_daemonreload':
+    command     => 'systemctl daemon-reload',
+    path        => [ '/bin/', '/usr/bin/' ],
+    refreshonly => true,
+  }
 }
