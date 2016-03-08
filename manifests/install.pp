bf64c05de694fafac542e6195576236a095cf05b
Lev Popov <lev@spotify.com>
2016-03-09 01:55:25 +0300

    ubuntu support for autostart, puppet 6 compatibility

--- manifests/install.pp
+++ manifests/install.pp
@@ -18,29 +18,55 @@ class ipset::install {
   ipset::install::helper_script { ['ipset_sync', 'ipset_init']: }
 
   # autostart
-  if $::osfamily == 'RedHat' and $::operatingsystemmajrelease == '6' {
-    # upstart starter
-    file { '/etc/init/ipset.conf':
-      owner   => 'root',
-      group   => 'root',
-      mode    => '0644',
-      content => template("${module_name}/init.upstart.erb"),
+  $warn = 'Autostart of ipset not implemented for this OS.'
+  case $::osfamily {
+    'RedHat': {
+      case $::operatingsystemmajrelease {
+        '6': {
+          # upstart starter
+          file { '/etc/init/ipset.conf':
+            owner   => 'root',
+            group   => 'root',
+            mode    => '0644',
+            content => template("${module_name}/init.upstart.erb"),
+          }
+        }
+        '7': {
+          # systemd
+          file { '/usr/lib/systemd/system/ipset.service':
+            owner   => 'root',
+            group   => 'root',
+            mode    => '0644',
+            content => template("${module_name}/systemd.service.erb"),
+          }
+          file { '/etc/systemd/system/basic.target.wants/ipset.service':
+            ensure => link,
+            target => '/usr/lib/systemd/system/ipset.service',
+          }
+        }
+        default: {
+          warning($warn)
+        }
+      }
     }
-  } else {
-    if $::osfamily == 'RedHat' and $::operatingsystemmajrelease == '7' {
-      # systemd
-      file { '/usr/lib/systemd/system/ipset.service':
-        owner   => 'root',
-        group   => 'root',
-        mode    => '0644',
-        content => template("${module_name}/systemd.service.erb"),
+    'Debian': {
+      case $::operatingsystem {
+        'Ubuntu': {
+          # upstart starter
+          file { '/etc/init/ipset.conf':
+            owner   => 'root',
+            group   => 'root',
+            mode    => '0644',
+            content => template("${module_name}/init.upstart.erb"),
+          }
+        }
+        default: {
+          warning($warn)
+        }
       }
-      file { '/etc/systemd/system/basic.target.wants/ipset.service':
-        ensure => link,
-        target => '/usr/lib/systemd/system/ipset.service',
-      }
-    } else {
-      warning('Autostart of ipset not implemented for this OS.')
+    }
+    default: {
+      warning($warn)
     }
   }
 }
