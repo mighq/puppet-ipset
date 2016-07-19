e2ae04dfacc4d96303095bd7ae1c51a6067b6516
Michal Zubac <zubac@avast.com>
2016-07-19 19:30:21 +0200

    support for rhel 7 + service deps

--- manifests/install.pp
+++ manifests/install.pp
@@ -18,36 +18,55 @@ class ipset::install {
   ipset::install::helper_script { ['ipset_sync', 'ipset_init']: }
 
   # autostart
-  if $::osfamily == 'RedHat' and $::operatingsystemmajrelease == '6' {
-    # upstart starter
-    file { '/etc/init/ipset.conf':
-      owner   => 'root',
-      group   => 'root',
-      mode    => '0644',
-      content => template("${module_name}/init.upstart.erb"),
-    }
-    # do not use original RC start script from the ipset package
-    # it is hard to define dependencies there
-    # also, it can collide with what we define through puppet
-    service { 'ipset':
-      enable   => false,
-      provider => 'redhat',
-    }
-  } else {
-    if $::osfamily == 'RedHat' and $::operatingsystemmajrelease == '7' {
-      # systemd
+  if $::osfamily == 'RedHat' {
+    if $::operatingsystemmajrelease == '6' {
+      # do not use original RC start script from the ipset package
+      # it is hard to define dependencies there
+      # also, it can collide with what we define through puppet
+      #
+      # using exec instead of Service, because of bug:
+      # https://tickets.puppetlabs.com/browse/PUP-6516
+      exec { 'ipset_disable_distro':
+        command  => "/bin/bash -c '/etc/init.d/ipset stop && /sbin/chkconfig ipset off'",
+        unless   => "/bin/bash -c '/sbin/chkconfig | /bin/grep ipset | /bin/grep -qv :on'",
+      }
+      ->
+      # upstart starter
+      file { '/etc/init/ipset.conf':
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0644',
+        content => template("${module_name}/init.upstart.erb"),
+      }
+      ~>
+      # upstart service autostart
+      service { 'ipset_enable_upstart':
+        name     => 'ipset',
+        enable   => true,
+        provider => 'upstart',
+      }
+      # dependency is covered by running ipset before RC scripts suite, where firewall service is
+    } elsif $::operatingsystemmajrelease == '7' {
+      # for management of dependencies
+      $firewall_service = $::ipset::params::firewall_service
+
+      # systemd service definition, there is no script in COS7
       file { '/usr/lib/systemd/system/ipset.service':
         owner   => 'root',
         group   => 'root',
         mode    => '0644',
-        content => template("${module_name}/systemd.service.erb"),
+        content => template("${module_name}/init.systemd.erb"),
       }
-      file { '/etc/systemd/system/basic.target.wants/ipset.service':
-        ensure => link,
-        target => '/usr/lib/systemd/system/ipset.service',
+      ~>
+      # systemd service autostart
+      service { 'ipset':
+        ensure => 'running',
+        enable => true,
       }
     } else {
-      warning('Autostart of ipset not implemented for this OS.')
+      warning('Autostart of ipset not implemented for this RedHat release.')
     }
+  } else {
+    warning('Autostart of ipset not implemented for this OS.')
   }
 }
--- manifests/params.pp
+++ manifests/params.pp
@@ -1,4 +1,8 @@
-class ipset::params {
+class ipset::params (
+  # if uou use "firewalld" to manage firewall,
+  # create instance of ipset::params class with this service name
+  $use_firewall_service = undef, 
+) {
   $package = $::osfamily ? {
     'RedHat' => 'ipset',
     default  => 'ipset',
@@ -8,5 +12,22 @@ class ipset::params {
     'RedHat' => '/etc/sysconfig/ipset.d',
     'Debian' => '/etc/ipset.d',
     default  => '/etc/ipset.d',
+  }
+
+  if $use_firewall_service {
+    # use specified override
+    $firewall_service = $use_firewall_service
+  } else {
+    # OS defaults
+    if $::osfamily == 'RedHat' {
+      if  $::operatingsystemmajrelease == '6' {
+        $firewall_service = 'iptables'
+      } elsif $::operatingsystemmajrelease == '7' {
+        $firewall_service = 'firewalld'
+      }
+    } else {
+      # by default expect everyone to use iptables
+      $firewall_service = 'iptables'
+    }
   }
 }
